26a9b3ec34305c2c0ce1e8664eb1ef6a2e376afd
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-22 17:25:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+    component: {
+        AppTitle,
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+    components: { AppTitle }
+}
+</script>
+
+<template>
+    <AppTitle />
+</template>
+
+<style scoped></style>
