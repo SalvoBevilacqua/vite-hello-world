ce4a46af5d051f0b9fff5c90f3bb39ead296b177
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-22 17:48:31 +0100

    fix:class

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -2,17 +2,15 @@
 export default {
     data() {
         return {
-
         }
     },
     methods: {
-
-    },
+    }
 }
 </script>
 
 <template>
-    <h1>La mia prima app con Vite!</h1>
+    <h1 class="position-absolute top-50 start-50 translate-middle">La mia prima app con Vite!</h1>
 </template>
 
 <style></style>
